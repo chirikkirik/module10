8e7cc4ab9ee412c22594fbc1af3114dff8ce2c77
chirikkirik <149368902+chirikkirik@users.noreply.github.com>
2024-10-17 16:04:13 +0300

    Add files via upload
    Домашнее задание по теме "Создание потоков".

---
+++ hw1.py
@@ -0,0 +1,35 @@
+import time
+import threading
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w') as f:
+        for i in range(1, word_count + 1):
+            f.write(f'Какое-то слово №{i}\n')
+            time.sleep(0.1)
+    print(f'Завершилась запись в файл{file_name}')
+
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+threads = []
+threads.append(threading.Thread(target=write_words, args=(10, 'example5.txt')))
+threads.append(threading.Thread(target=write_words, args=(30, 'example6.txt')))
+threads.append(threading.Thread(target=write_words, args=(200, 'example7.txt')))
+threads.append(threading.Thread(target=write_words, args=(100, 'example8.txt')))
+
+
+start_time = time.time()
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+end_time = time.time()
+print(f"Завершилось выполнение всех потоков. Общее время выполнения: {end_time - start_time:.2f} секунд")
+
+
+
